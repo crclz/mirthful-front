ccc556f0ab8a5a4b67ba9b17c57c4d18572e88e7
crclz <crucialize@foxmail.com>
2020-05-27 16:10:18 +0800

    hot information for home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NotificationService } from '../notification.service';
 import { Subject, Observable } from 'rxjs';
-import { QWork, QTopic, QPost, QDiscussion, WorkService, WorkType, TopicService } from 'src/openapi';
+import { QWork, QTopic, QPost, QDiscussion, WorkService, WorkType, TopicService, QComment, CommentsService } from 'src/openapi';
 import { shareReplay, switchMap } from 'rxjs/operators';
 
 @Component({
@@ -23,10 +23,17 @@ export class HomeComponent implements OnInit {
   posts$: Observable<QPost[]>;
   discussions$: Observable<QDiscussion[]>;
 
+
+  hotBooks$: Observable<QWork[]>;
+  hotFilms$: Observable<QWork[]>;
+  hotComments$: Observable<QComment[]>;
+  hotTopics$: Observable<QTopic[]>;
+
   constructor(
     public notification: NotificationService,
     private workApi: WorkService,
-    private topicApi: TopicService
+    private topicApi: TopicService,
+    private commentApi: CommentsService
   ) { }
 
   ngOnInit(): void {
@@ -59,6 +66,27 @@ export class HomeComponent implements OnInit {
       switchMap(word => this.topicApi.searchDiscussions(word, 0)),
       shareReplay(1)
     );
+
+
+    //#region hot module
+
+    this.hotBooks$ = this.workApi.hotestWorks(WorkType.Book).pipe(shareReplay(1));
+
+    this.hotFilms$ = this.workApi.hotestWorks(WorkType.Film).pipe(shareReplay(1));
+
+    this.hotComments$ = this.commentApi.hotestComments().pipe(shareReplay(1));
+
+    this.hotTopics$ = this.topicApi.hotestTopicsNotGroup().pipe(shareReplay(1));
+
+    //#endregion
+  }
+
+  genlist(k: number) {
+    let list = [];
+    for (let i = 0; i < k; i++) {
+      list.push(i);
+    }
+    return list;
   }
 
   goSearch(word: string) {
